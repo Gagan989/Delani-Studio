0191af15f3f6a7274d918adb46b5e7705d69a3f6
gagan chhabra <gsgagan85@gmail.com>
2021-04-26 11:08:04 -0400

    Add content to contact us and ass css styles to it

--- js/scrips.js
+++ js/scrips.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     whatwedo.forEach(function(x){
       $("#"+x+", #"+x+"p").click(function(){
         $("#"+x+", #"+x+"p").toggle();
-    )}:
+)}:
 
     var wrap = ['.img_wrap','.img_wrap1','.img_wrap2','.img_wrap3','.img_wrap4','.img_wrap5','.img_wrap6','.img_wrap7'];
    wrap.forEach(y => {
@@ -15,7 +15,16 @@ $(document).ready(function(){
          $(overlay[indexofwrap]).hide();
      })
      })
-   }):
+}):
 
-   
+$("#contactus").submit(function() {
+    var nameInput = $("input#name").val();
+
+    var emailInput = $("input#email").val();
+    var messageInput = $("textarea#message").val();
+
+    alert(nameInput + " we have received your message. Thank you for reaching out to us.");
+
+  })
+})
 
